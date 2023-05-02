5a885156b97549778d1dd99a456000f73292a768
PedroCamillozzi <130605829+PedroCamillozzi@users.noreply.github.com>
2023-05-03 00:09:56 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaPensarEj9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] triangulo = new string[4,8];
+            for (int i = 0; i <=4; i++)
+            { 
+                for (int j = 0; j<=8; j++)
+                {
+                    triangulo[i, j] = " ";
+                }
+            }
+
+        }
+    }
+}
